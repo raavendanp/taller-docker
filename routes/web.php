eabe85f18e0ce11bb55756d5bd63ff1804384856
unknown <raavendanp@eafit.edu.co>
2020-11-11 01:32:35 -0500

    a

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
     
 });
-$router->get('/public/random-images/', [
+$router->get('/random-images', [
     'as' => 'index', 'uses' => 'Controller@index'
    ]);
